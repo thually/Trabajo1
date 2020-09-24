8c3cc19624cc71c76723fc399a44771ed5bcb121
thually <alejosalazarmejia.2000@gmail.com>
2020-09-24 15:06:43 -0500

    Primer esquema SistemaZoologico.java

--- src/main/java/SistemaZoologico.java
+++ src/main/java/SistemaZoologico.java
@@ -1,3 +1,67 @@
+import org.json.simple.JSONObject;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SistemaZoologico {
+    public static Usuario usuarioActual = null;
+    public static Scanner input = new Scanner(System.in);
+    public static ArrayList<Usuario> usuarios = new ArrayList<>();
+
+    public static void main(String[] args) {
+        cargarUsuario();
+        label: while (true) {
+            System.out.println("----------------------------------");
+            System.out.println("Este es el sistema de Zoologicos. \nPor favor, selecione alguna de las siguientes opciones: ");
+            System.out.println("1. Ingresar.");
+            System.out.println("2. Registrarse. \n");
+            String opcion = input.next();
+            System.out.println();
+            switch (opcion){
+                case "1":
+                    if (usuarios.isEmpty()){
+                        System.out.println("Aun no se encuentran usuarios registrados");
+                        break;
+                    }
+                    ingresar(); //la unica forma de salir de este metodo, es que el usuario ingrese exitosamente.
+                    break label;
+                case "2":
+                    registrar(); //Una vez se registre, se vuelve a preguntar si desea ingresar.
+                    break;
+            }
+            System.out.println("----------------------------------");
+        }
+
+        //...continua el programa.
+    }
+
+    public static void cargarUsuario(){}
+
+    public static void ingresar(){
+        //Crea un usuario y agregalo a la lista para probar que funcione
+
+        label: while (true){
+            System.out.println("-------------------------");
+            System.out.println("Por favor ingrese documento o correo:");
+            String id = input.next();
+            System.out.println("Por favor ingrese su contrasenna:");
+            String contrasenna = input.next();
+            for (Usuario usuario : usuarios) {
+                if (usuario.Documento.equals(id) || usuario.Correo.equals(id)){
+                    if (usuario.Contrasenna.equals(contrasenna)){
+                        usuarioActual = usuario;
+                        break label;
+                    }
+                }
+            }
+            if (usuarioActual == null){
+                System.out.println("ERROR: los datos suministrados no coinciden con ninguno de los usuarios registrados");
+            }
+            System.out.println("-------------------------");
+        }
+    }
+
+    public static void registrar(){
+        //Dentro de este metodo se guardan los usuarios en usuarios.json.
+    }
 
 }
