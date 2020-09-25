ea98d608b9d94c83949c6b398ea2b9962428fd81
thually <alejosalazarmejia.2000@gmail.com>
2020-09-25 08:46:41 -0500

    pequeño cambio

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,7 +2,7 @@ public class Animal {
     int id;
     String especie; // Sea un Leon, foca, etc
     int nivelAgresividad; // Se mide del 0 al 5, siendo por ejemplo 0 un gorrion y 5 un leon
-    String alimentacion; // Carnivoro, Hervivoro, Omnivoro,etc
+    String alimentacion; // Carnivoro, Hervivoro, Omnivoro.
     Habitat habitat;
     ZooAmigo zooAmigo;
 
