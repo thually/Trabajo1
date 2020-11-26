526f39a02ad6d62d5ead0bfad95fe0079a427fc5
thually <alejosalazarmejia.2000@gmail.com>
2020-11-26 12:56:10 -0500

    Editar Relaciones Animal Finalizado CRUD

--- src/main/java/trabajo2/EditarAnimalController.java
+++ src/main/java/trabajo2/EditarAnimalController.java
@@ -1,4 +1,125 @@
 package trabajo2;
 
-public class EditarAnimalController {
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
+
+public class EditarAnimalController implements Initializable {
+
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private TextField especieTextField;
+    @FXML
+    private ChoiceBox<Integer> agresividadCB;
+    @FXML
+    private TextField alimentacionTextField;
+    @FXML
+    private ChoiceBox<Integer> idAnimalesDisp;
+    @FXML
+    private Label warning;
+
+    @FXML
+    private void editar(ActionEvent event) throws IOException{
+        if (idTextField.getText().trim().isEmpty() && especieTextField.getText().trim().isEmpty() && agresividadCB.getValue() == null
+                && alimentacionTextField.getText().trim().isEmpty()){
+            warning.setText("MENSAJE: Todos los campos se encuentran vacíos.");
+            return;
+        }
+        if (idAnimalesDisp.getValue() == null){
+            warning.setText("MENSAJE: Aún no selecciona el ID del Animal que desea editar.");
+            return;
+        }
+
+        Animal animalEditado = Animal.animalesPorID.get(idAnimalesDisp.getValue());
+
+        int id=0;
+        if (!idTextField.getText().trim().isEmpty()) {
+            try{
+                id = Integer.parseInt(idTextField.getText().trim());
+                if (Animal.animalesPorID.containsKey(id)){
+                    warning.setText("Ya existe un Animal con este ID registrado en el sistema");
+                    return;
+                }
+            }catch (Exception e){
+                warning.setText("El ID del Animal debe que ser un número entero");
+                return;
+            }
+        }
+
+        String especie = especieTextField.getText().trim().toLowerCase();
+        String alimentacion = alimentacionTextField.getText().trim().toLowerCase();
+
+        if (idTextField.getText().trim().isEmpty()){}
+        else {
+            Animal.animalesPorID.remove(animalEditado.id);
+            animalEditado.id = id;
+            Animal.animalesPorID.put(id, animalEditado);
+        }
+        if (agresividadCB.getValue() == null){}
+        else {
+            int nivelAgrecividad = agresividadCB.getValue();
+            Animal.animalesPorAgresividad.get(animalEditado.nivelAgresividad).remove(animalEditado);
+            animalEditado.nivelAgresividad = nivelAgrecividad;
+            if (!Animal.animalesPorAgresividad.containsKey(nivelAgrecividad)) {
+                Animal.animalesPorAgresividad.put(nivelAgrecividad, new LinkedList<>());
+            }
+            Animal.animalesPorAgresividad.get(nivelAgrecividad).add(animalEditado);
+        }
+        if (especie.isEmpty()){}
+        else {
+            Animal.animalesPorEspecie.get(animalEditado.especie).remove(animalEditado);
+            animalEditado.especie = especie;
+            if (!Animal.animalesPorEspecie.containsKey(especie)) {
+                Animal.animalesPorEspecie.put(especie, new LinkedList<>());
+            }
+            Animal.animalesPorEspecie.get(especie).add(animalEditado);
+        }
+        if (alimentacion.isEmpty()){}
+        else animalEditado.alimentacion = alimentacion;
+
+        warning.setText("");
+
+        idTextField.setText("");
+        especieTextField.setText("");
+        alimentacionTextField.setText("");
+        idAnimalesDisp.setValue(null);
+        agresividadCB.setValue(null);
+        idAnimalesDisp.setItems(FXCollections.observableList(new ArrayList<>(Animal.animalesPorID.keySet())));
+
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText("Edición satisfactoria");
+        alert.setContentText("Se ha editado el Animal satisfactoriamente:\n" + animalEditado);
+        alert.showAndWait();
+
+    }
+
+    @FXML
+    private void volver(ActionEvent event) throws IOException {
+        App.setRoot("administrarAnimal");
+    }
+
+    @FXML
+    private void rutaEditarRelaciones(ActionEvent event) throws IOException {
+        App.setRoot("editarRelacionesAnimal");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        idAnimalesDisp.setItems(FXCollections.observableList(new ArrayList<>(Animal.animalesPorID.keySet())));
+        agresividadCB.setItems(FXCollections.observableList(new ArrayList<>(Arrays.asList(1,2,3,4,5))));
+    }
 }
--- src/main/java/trabajo2/EditarRelacionesAnimalController.java
+++ src/main/java/trabajo2/EditarRelacionesAnimalController.java
@@ -1,4 +1,165 @@
 package trabajo2;
 
-public class EditarRelacionesAnimalController {
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import org.jgrapht.Graphs;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class EditarRelacionesAnimalController implements Initializable {
+
+    @FXML
+    public Label Label1;
+
+    @FXML
+    private Button crearRelacion;
+
+    @FXML
+    public Button eliminarRelacion;
+
+    @FXML
+    private ChoiceBox<Integer> idAnimalesDisp;
+
+    @FXML
+    private ChoiceBox<String> accion;
+
+    @FXML
+    private ChoiceBox<Integer> idParaEditarDisp;
+
+    @FXML
+    private Label warning;
+
+    @FXML
+    private void elegirEliminarEditar(ActionEvent event) throws IOException {
+        warning.setText("");
+
+        if (accion.getValue() == null) {
+            return;
+        }
+        Animal animalEditado = Animal.animalesPorID.get(idAnimalesDisp.getValue());
+        if (accion.getValue().equals("Eliminar")){
+            ArrayList<Object> objAdyacentes = new ArrayList<>(Graphs.neighborListOf(App.sistemaZoo, animalEditado));
+            ArrayList<Integer> IDparaEliminar = new ArrayList<>();
+            for (Object obj : objAdyacentes){
+                if (obj instanceof Habitat){
+                    Habitat aux = (Habitat) obj;
+                    IDparaEliminar.add(aux.id);
+                }
+            }
+            if (IDparaEliminar.isEmpty()){
+                warning.setText("MENSAJE: Este Animal no tiene relaciones existentes con ningún Habitat.");
+                return;
+            }
+
+            Label1.setText("Finalmente, selecciones el ID del Animal con el cual quiere eliminar la relación:");
+            idParaEditarDisp.setItems(FXCollections.observableList(IDparaEliminar));
+            idParaEditarDisp.setVisible(true);
+            eliminarRelacion.setVisible(true);
+            crearRelacion.setVisible(false);
+
+
+        } else if (accion.getValue().equals("Crear")){
+            ArrayList<Object> objAdyacentes = new ArrayList<>(Graphs.neighborListOf(App.sistemaZoo, animalEditado));
+            ArrayList<Integer> IDparaEliminar = new ArrayList<>();
+            for (Object obj : objAdyacentes){
+                if (obj instanceof Habitat){
+                    Habitat aux = (Habitat) obj;
+                    IDparaEliminar.add(aux.id);
+                }
+            }
+
+            ArrayList<Integer> IDDispParaCrear = new ArrayList<>();
+            for (int idDis : Habitat.habitatsPorID.keySet()){
+                if (!IDparaEliminar.contains(idDis)){
+                    IDDispParaCrear.add(idDis);
+                }
+            }
+            if (IDDispParaCrear.isEmpty()){
+                warning.setText("MENSAJE: No hay Habitats disponibles para crear esta nueva relación.");
+                return;
+            }
+
+            idParaEditarDisp.setItems(FXCollections.observableList(IDDispParaCrear));
+
+            Label1.setText("Finalmente, selecciones el ID del Habita con el cual quiere crear la nueva relación:");
+            idParaEditarDisp.setVisible(true);
+            crearRelacion.setVisible(true);
+            eliminarRelacion.setVisible(false);
+        }
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        idAnimalesDisp.setItems(FXCollections.observableList(new ArrayList<>(Animal.animalesPorID.keySet())));
+    }
+
+    @FXML
+    private void setAccionIDHab(ActionEvent event) throws IOException {
+        accion.setItems(FXCollections.observableList(new ArrayList<>(Arrays.asList("Crear", "Eliminar"))));
+        warning.setText("");
+        Label1.setText("");
+        idParaEditarDisp.setVisible(false);
+        eliminarRelacion.setVisible(false);
+        crearRelacion.setVisible(false);
+        warning.setText("");
+
+    }
+
+    @FXML
+    private void volver(ActionEvent event) throws IOException {
+        App.setRoot("editarAnimal");
+    }
+
+    @FXML
+    public void AccionEliminarRelacion(ActionEvent event) throws IOException {
+        if (idParaEditarDisp.getValue() == null){
+            warning.setText("MENSAJE: Último campo vacío.");
+            return;
+        }
+        int idHabitat = idParaEditarDisp.getValue();
+        Habitat habitat =Habitat.habitatsPorID.get(idHabitat);
+        Animal animaEditado = Animal.animalesPorID.get(idAnimalesDisp.getValue());
+        App.sistemaZoo.removeEdge(habitat, animaEditado);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText("Eliminación satisfactoria");
+        alert.setContentText("Se ha eliminado la relación de el Animal satisfactoriamente:\n");
+        alert.showAndWait();
+        App.setRoot("editarAnimal");
+    }
+
+    @FXML
+    public void AccionCrearRelacion(ActionEvent event) throws IOException {
+        if (idParaEditarDisp.getValue() == null){
+            warning.setText("MENSAJE: Último campo vacío.");
+            return;
+        }
+        int idHabitat = idParaEditarDisp.getValue();
+        Habitat habitat =Habitat.habitatsPorID.get(idHabitat);
+        Animal animalEditado = Animal.animalesPorID.get(idAnimalesDisp.getValue());
+        // El siguiente for es para asegurarnos que un animal solo puede estar relacionado con un habitat
+        for (Habitat hab : Habitat.habitatsPorID.values()){
+            if (App.sistemaZoo.containsEdge(animalEditado, hab)){
+                App.sistemaZoo.removeEdge(animalEditado, hab);
+            }
+        }
+        App.sistemaZoo.addEdge(habitat, animalEditado);
+
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText("Creación satisfactoria");
+        alert.setContentText("Se ha creado la nueva relación con el Habitat satisfactoriamente:\n");
+        alert.showAndWait();
+        App.setRoot("editarAnimal");
+    }
 }
